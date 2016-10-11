c46442ab063dbe79a47ff9cb5e902a5566442242
thomas-samblanet <tsamblan@lesley.edu>
2016-10-10 20:49:04 -0400

    Task 7,8&12

---
+++ js.js
@@ -0,0 +1,7 @@
+Array.prototype.random = function (length) {
+    return this[Math.floor((Math.random()*length))];
+};
+
+var fruit = ['strawberry', 'mulberry', 'tangerine', 'overpriced organic grapes', 'grape soda', 'lime'];
+var chosen_fruit = fruit.random(fruit.length);
+document.write("<p>" + chosen_fruit + "</p>");

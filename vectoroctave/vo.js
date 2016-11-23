085fbdec0b53a2acc55809975936d76720c2347c
rpuorro <rpuorro@lesley.edu>
2016-11-23 01:38:59 -0500

    sonic the soundcolor

--- vectoroctave/vo.js
+++ vectoroctave/vo.js
@@ -9,25 +9,25 @@ function init() {
     stage.enableMouseOver(10);
 
     //these are variables for the elements that contain the music sounds within artboards. Per CreatejsAPI, originally used as text elements hence "label".
-    var label1 = new createjs.Text("g", "200px webdings", "#ECEFF1");
+    var label1 = new createjs.Text("g", "200px webdings", "#311B92");
     label1.x = 400;
     label1.y = 50;
-    label1.alpha = 0.5;
+    label1.alpha = 1;
 
-    var label2 = new createjs.Text("g", "200px webdings", "#ECEFF1");
+    var label2 = new createjs.Text("g", "200px webdings", "#311B92");
     label2.x = 400;
     label2.y = 400;
-    label2.alpha = 0.5;
+    label2.alpha = 1;
 
-    var label3 = new createjs.Text("g", "200px webdings", "#ECEFF1");
+    var label3 = new createjs.Text("g", "200px webdings", "#311B92");
     label3.x = 700;
     label3.y = 50;
-    label3.alpha = 0.5;
+    label3.alpha = 1;
 
-    var label4 = new createjs.Text("g", "200px webdings", "#ECEFF1");
+    var label4 = new createjs.Text("g", "200px webdings", "#311B92");
     label4.x = 700;
     label4.y = 400;
-    label4.alpha = 0.5;
+    label4.alpha = 1;
 
 
 // each variable needs its own function, because every trigger-able element within the artboard contains a unique musical note-IE handleInteraction is the function of label1, when label1 is passed over, moog10 synth plays
@@ -51,46 +51,49 @@ function init() {
 }
 
 function handleInteraction(event) {
-    event.target.alpha = (event.type == "mousedown") ? 1 : 0.5;
+    event.target.alpha = (event.type == "mouseover") ? 1 : 0.5;
     var audio = new Audio('jeff-the-brotherhood_moog-14_synths_one_shot_.wav.wav');
     audio.play();
 }
 function nextHandle(event) {
-        event.target.alpha = (event.type == "mousedown") ? 1 : 0.5;
-        var audio = new Audio('jeff-the-brotherhood_moog-9_synths_one_shot_.wav.wav');
-        audio.play();
-    }
+    event.target.alpha = (event.type == "mouseover") ? 1 : 0.5;
+    var audio = new Audio('jeff-the-brotherhood_moog-9_synths_one_shot_.wav.wav');
+    audio.play();
+}
 function thirdHandle(event) {
-    event.target.alpha = (event.type == "mousedown") ? 1 : 0.5;
+    event.target.alpha = (event.type == "mouseover") ? 1 : 0.5;
     var audio = new Audio('jeff-the-brotherhood_moog-10_synths_one_shot_.wav.wav');
     audio.play();
 }
 function fourthHandle(event) {
-    event.target.alpha = (event.type == "mousedown") ? 1 : 0.5;
+    event.target.alpha = (event.type == "mouseover") ? 1 : 0.5;
     var audio = new Audio('jeff-the-brotherhood_moog-11_synths_one_shot_.wav.wav');
     audio.play();
+    audio.w(25)
 }
-var radius = 1;
+var radius = .5;
 var dragging = false;
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-context.lineWidth = 2;
+context.lineWidth = 1;
 
 var putPoint = function (brush) {
-  if (dragging) {
-    context.lineTo(brush.clientX, brush.clientY);
-    context.stroke();
-    context.beginPath();
-    context.arc(brush.clientX, brush.clientY, radius, 0, Math.PI*2);
-    context.fill();
-    context.beginPath();
-    context.moveTo(brush.clientX, brush.clientY);
-   // context.fillText("......",brush.clientX, brush.clientY);
-    //context.rotate( Math.PI );
-      context.globalAlpha=.4
-  }
+    if (dragging) {
+        context.lineTo(brush.clientX, brush.clientY);
+        context.stroke();
+        context.beginPath();
+        context.arc(brush.clientX, brush.clientY, radius, 0, Math.PI*2);
+
+        context.fill();
+        context.beginPath( brush.clientY);
+        context.beginPath();
+        context.moveTo(brush.clientX, brush.clientY);
+        context.fillText("THIS",brush.clientX, brush.clientY);
+        context.rotate(Math.PI*2);
+        // context.opacity=.1
+    }
 };
 var engage = function () {
     dragging = true

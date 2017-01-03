750f9a9f5391890647db9001f40c962859539ef8
rpuorro <rpuorro@lesley.edu>
2017-01-03 08:54:12 -0500

    restructuring files

--- 04 javascript review/fruit.js
+++
@@ -1,9 +0,0 @@
-// selecting a random array item
-
-Array.prototype.random = function (length) {
-    return this[Math.floor((Math.random()*length))];
-};
-
-var fruit = ['strawberry', 'mulberry', 'tangerine', 'overpriced organic grapes', 'grape soda', 'lime'];
-var chosen_fruit = fruit.random(fruit.length);
-document.write("<p>" + chosen_fruit + "</p>");
